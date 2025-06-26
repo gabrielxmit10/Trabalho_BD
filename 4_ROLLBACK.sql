ebf8e03c77c41cb27a22534cff6932444689a664
Gabriel Schmitz <138041772+gabrielxmit10@users.noreply.github.com>
2025-06-26 19:37:55 -0300

    Update 4_ROLLBACK.sql

--- 4_ROLLBACK.sql
+++ 4_ROLLBACK.sql
@@ -1,27 +1,22 @@
--- Usar o banco de dados correto
 USE VacinacaoDB;
 GO
 
--- 1. ANTES: Verificando a quantidade original de doses
--- Usando um código de lote que existe na sua base de dados
+-- 1. ANTES: Qt_Doses Original
 SELECT Cd_Lote, Qt_Doses FROM dbo.Lote WHERE Cd_Lote = 'LOT-CVF01';
 GO
 
--- 2. DURANTE: Iniciando a transação e fazendo a atualização
 BEGIN TRANSACTION;
 
-    -- Tenta atualizar a quantidade de doses para um valor incorreto
     UPDATE dbo.Lote
     SET Qt_Doses = 999
     WHERE Cd_Lote = 'LOT-CVF01';
 
-    -- Verificando o dado DENTRO da transação
+    -- 2. DURANTE COMMIT: Qt_Doses nova
     SELECT Cd_Lote, Qt_Doses FROM dbo.Lote WHERE Cd_Lote = 'LOT-CVF01';
 
--- 3. ROLLBACK: Desfazendo a alteração
 ROLLBACK TRANSACTION;
 GO
 
--- 4. DEPOIS: Verificando se a alteração foi desfeita
+-- 3. DEPOIS: Qt_Doses apÃ³s Rollback (original)
 SELECT Cd_Lote, Qt_Doses FROM dbo.Lote WHERE Cd_Lote = 'LOT-CVF01';
 GO
