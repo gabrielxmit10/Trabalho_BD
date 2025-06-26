42369cec9b17a06bc2357d0c381530de203c68ff
Gabriel Schmitz <138041772+gabrielxmit10@users.noreply.github.com>
2025-06-26 20:20:31 -0300

    Update 13_CURSOR_RELATORIO_VACS.sql

--- 13_CURSOR_RELATORIO_VACS.sql
+++ 13_CURSOR_RELATORIO_VACS.sql
@@ -1,17 +1,16 @@
 USE VacinacaoDB;
 GO
 
--- 1. DECLARAR AS VARIÁVEIS
--- Estas variáveis irão guardar os valores de cada linha enquanto o cursor percorre os dados.
+-- 1. VARIAVEIS pra guardar valores enquanto o cursor passa
 DECLARE @DtVacinacao DATETIME;
 DECLARE @NmPaciente CHAR(150);
 DECLARE @CdLote CHAR(10);
 DECLARE @NmFabricante CHAR(100);
 
--- 2. DECLARAR O CURSOR
--- Associamos o cursor a uma consulta SELECT que junta as tabelas necessárias.
-PRINT '--- Relatório de Vacinação com Cursor ---';
+
+PRINT '--- VacinaÃ§Ãµes Registradas com Cursor ---';
 PRINT '-----------------------------------------------------------------------------------';
+-- 2. DECLARE do CURSOR
 DECLARE cursor_relatorio_vacinacao CURSOR FOR
 SELECT
     vr.Dt_Vacinacao,
@@ -29,29 +28,22 @@ JOIN
 ORDER BY
     vr.Dt_Vacinacao;
 
--- 3. ABRIR O CURSOR
--- A consulta SELECT é executada e os resultados são armazenados na memória, prontos para serem percorridos.
+-- OPEN do CURSOR
 OPEN cursor_relatorio_vacinacao;
 
--- 4. PERCORRER (FETCH) O CURSOR
--- Pegamos a primeira linha do cursor e a colocamos nas nossas variáveis.
 FETCH NEXT FROM cursor_relatorio_vacinacao INTO @DtVacinacao, @NmPaciente, @CdLote, @NmFabricante;
 
--- O laço WHILE continua enquanto a última operação FETCH for bem-sucedida (@@FETCH_STATUS = 0)
+-- WHILE enquanto FETCH funcionar
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    -- Dentro do laço, trabalhamos com os dados da linha atual que estão nas variáveis.
-    -- Usamos RTRIM() para remover os espaços em branco extras dos campos CHAR.
-    PRINT 'Vacinação registrada para ' + RTRIM(@NmPaciente) + ' em ' + CONVERT(CHAR(10), @DtVacinacao, 103) + '. (Lote: ' + RTRIM(@CdLote) + ' / Fabricante: ' + RTRIM(@NmFabricante) + ')';
-    --PRINT '-----------------------------------------------------------------------------------'
-    -- No final do laço, pegamos a próxima linha para a próxima iteração.
+    PRINT 'VacinaÃ§Ã£o registrada para ' + RTRIM(@NmPaciente) + ' em ' + CONVERT(CHAR(10), @DtVacinacao, 103) + '. (Lote: ' + RTRIM(@CdLote) + ' / Fabricante: ' + RTRIM(@NmFabricante) + ')';
+    -- PRINT '-----------------------------------------------------------------------------------'
     FETCH NEXT FROM cursor_relatorio_vacinacao INTO @DtVacinacao, @NmPaciente, @CdLote, @NmFabricante;
 END;
 
--- 5. FECHAR E DESALOCAR O CURSOR
--- Após o fim do laço, é crucial fechar e desalocar o cursor para liberar os recursos do servidor.
+-- 3. CLOSE e DEALLOCATE do CURSOR (liberando recursos)
 CLOSE cursor_relatorio_vacinacao;
 DEALLOCATE cursor_relatorio_vacinacao;
 GO
 
-PRINT '--- Fim do Relatório ---';
+PRINT '--- Fim ---';
