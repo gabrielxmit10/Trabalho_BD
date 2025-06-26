ebe9274c992d24d567ff03ee0e75adfa907e139f
Gabriel Schmitz <138041772+gabrielxmit10@users.noreply.github.com>
2025-06-26 19:54:40 -0300

    Update 8_CREATE_TRIGGER.sql

--- 8_CREATE_TRIGGER.sql
+++ 8_CREATE_TRIGGER.sql
@@ -1,11 +1,8 @@
--- Usar o banco de dados correto
 USE VacinacaoDB;
 GO
 
--- Passo 1: Adicionar a coluna para armazenar a data da última vacinação na tabela Paciente.
--- Usamos IF NOT EXISTS para que o script possa ser executado várias vezes sem erro.
-PRINT '--- Adicionando a coluna Dt_Ultima_Vacinacao à tabela Paciente... ---';
 
+-- Criar Coluna pra Ãšltima VacinaÃ§Ã£o do Paciente
 IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE Name = N'Dt_Ultima_Vacinacao' AND Object_ID = Object_ID(N'dbo.Paciente'))
 BEGIN
     ALTER TABLE dbo.Paciente
@@ -13,32 +10,27 @@ BEGIN
 END
 GO
 
--- Passo 2: Criar o TRIGGER
--- Primeiro, apagamos o TRIGGER se ele já existir.
+
 DROP TRIGGER IF EXISTS dbo.trgAtualizaUltimaVacinacao;
 GO
 
-PRINT '--- Criando o TRIGGER dbo.trgAtualizaUltimaVacinacao... ---';
-GO
-
+-- 1. Criar o TRIGGER (dbo.trgAtualizaUltimaVacinacao)
 CREATE TRIGGER dbo.trgAtualizaUltimaVacinacao
-ON dbo.Vacinacao_Registro -- O gatilho pertence à tabela Vacinacao_Registro
-FOR INSERT -- E ele dispara no evento de INSERT
+ON dbo.Vacinacao_Registro
+FOR INSERT -- O Gatilho Ã© o Insert
 AS
 BEGIN
-    -- Impede que mensagens como "(1 linha afetada)" apareçam no resultado
-    SET NOCOUNT ON;
+    -- SET NOCOUNT ON;
 
-    -- Atualiza a tabela Paciente
     UPDATE dbo.Paciente
     SET
-        -- Define a nova data com base na data da linha que acabou de ser inserida
         Dt_Ultima_Vacinacao = i.Dt_Vacinacao
     FROM
         dbo.Paciente AS p
     JOIN
-        -- 'inserted' é uma tabela "mágica" que existe dentro de um trigger
-        -- e contém os dados da(s) linha(s) que acabaram de ser inseridas.
         inserted AS i ON p.Cd_Paciente = i.Cd_Paciente;
 END
+GO
+
+PRINT 'Trigger criado com Sucesso';
 GO
