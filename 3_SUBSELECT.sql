6a93551e21b03df0450bc8d2538d8410a0332718
Gabriel Schmitz <138041772+gabrielxmit10@users.noreply.github.com>
2025-06-26 19:35:43 -0300

    Update 3_SUBSELECT.sql

--- 3_SUBSELECT.sql
+++ 3_SUBSELECT.sql
@@ -1,36 +1,38 @@
 USE VacinacaoDB;
 GO
 
--- 1. ANTES: Sexo Original (M)
-SELECT Nm_Paciente, Cd_Sexo FROM dbo.Paciente WHERE Cd_Paciente = 'PAC-002';
+-- Pacientes q tomaram vacina de TODO fabricante
+SELECT
+    p.Nm_Paciente,
+    p.Cd_Paciente
+FROM
+    dbo.Paciente AS p
+JOIN
+    dbo.Vacinacao_Registro AS vr ON p.Cd_Paciente = vr.Cd_Paciente
+JOIN
+    dbo.Lote AS l ON vr.Cd_Lote = l.Cd_Lote
+GROUP BY
+    p.Cd_Paciente, p.Nm_Paciente
+HAVING
+    COUNT(DISTINCT l.Cd_Fabricante) = (SELECT COUNT(*) FROM dbo.Fabricante);
 GO
 
-BEGIN TRANSACTION;
-
-    UPDATE dbo.Paciente
-    SET Cd_Sexo = 'O'
-    WHERE Cd_Paciente = 'PAC-002';
-
-    -- 2. DURANTE COMMIT: Sexo novo não confirmado ainda (O)
-    SELECT Nm_Paciente, Cd_Sexo FROM dbo.Paciente WHERE Cd_Paciente = 'PAC-002';
-
-COMMIT TRANSACTION;
-GO
-
--- 3. DEPOIS: Sexo novo já alterado (O)
-SELECT Nm_Paciente, Cd_Sexo FROM dbo.Paciente WHERE Cd_Paciente = 'PAC-002';
-GO
-
-
--------------------------------------------------------
-
-
--- Mudando de volta pra poder rodar de novo
-BEGIN TRANSACTION;
-
-    UPDATE dbo.Paciente
-    SET Cd_Sexo = 'M'
-    WHERE Cd_Paciente = 'PAC-002';
-
-COMMIT TRANSACTION;
+-- Provando Fabricantes da Helena Costa
+SELECT
+    p.Nm_Paciente,
+    f.Nm_Fabricante,
+    v.Nm_Vacina,
+    vr.Dt_Vacinacao
+FROM
+    dbo.Paciente AS p
+JOIN
+    dbo.Vacinacao_Registro AS vr ON p.Cd_Paciente = vr.Cd_Paciente
+JOIN
+    dbo.Lote AS l ON vr.Cd_Lote = l.Cd_Lote
+JOIN
+    dbo.Fabricante AS f ON l.Cd_Fabricante = f.Cd_Fabricante
+JOIN
+    dbo.Vacina AS v ON l.Cd_Vacina = v.Cd_Vacina
+WHERE
+    p.Cd_Paciente = 'PAC-005'; -- Só ver a Helena Costa
 GO
