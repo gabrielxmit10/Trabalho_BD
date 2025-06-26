388aac3b9758fab32199453f6f48064723cec2dd
Gabriel Schmitz <138041772+gabrielxmit10@users.noreply.github.com>
2025-06-26 19:39:14 -0300

    Update 5_ERRO_FOREIGN_KEY.sql

--- 5_ERRO_FOREIGN_KEY.sql
+++ 5_ERRO_FOREIGN_KEY.sql
@@ -1,15 +1,14 @@
 USE VacinacaoDB;
 GO
 
--- 1. Verificar quais fabricantes existem atualmente na tabela.
--- Nenhum com o código 'FAB-XXX'.
+-- 1. Fabricantes do Banco de Dados
 SELECT 
     *
 FROM 
     dbo.Fabricante;
 GO
 
--- 2. Tentativa de inserir um novo Lote com um Cd_Fabricante que NÃO EXISTE (código 'FAB-XXX').
+-- 2. Insert com Cd_Fabricante que NÃƒO EXISTE (cÃ³digo 'FAB-XXX').
 INSERT INTO dbo.Lote 
     (Cd_Lote, Cd_Vacina, Cd_Fabricante, Dt_Validade, Dt_Fabricacao, Qt_Doses)
 VALUES 
