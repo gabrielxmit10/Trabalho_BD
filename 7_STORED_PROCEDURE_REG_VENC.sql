b007eade6990990ea5c50a1b6b9b4bf25ff33f44
Gabriel Schmitz <138041772+gabrielxmit10@users.noreply.github.com>
2025-06-26 19:49:20 -0300

    Update 7_STORED_PROCEDURE_REG_VENC.sql

--- 7_STORED_PROCEDURE_REG_VENC.sql
+++ 7_STORED_PROCEDURE_REG_VENC.sql
@@ -1,33 +1,29 @@
--- Usar o banco de dados correto
 USE VacinacaoDB;
 GO
 
--- Passo 1: Preparar os dados com formato de data universal (não ambíguo)
-PRINT '--- Inserindo dados de teste com formato de data correto... ---';
 
+-- 1. Inserindo o Lote Vencido (da QUESTÃƒO 6, caso fora de ordem)
 IF NOT EXISTS (SELECT 1 FROM dbo.Lote WHERE Cd_Lote = 'LOT-VENC')
 BEGIN
     INSERT INTO dbo.Lote (Cd_Lote, Cd_Vacina, Cd_Fabricante, Dt_Validade, Dt_Fabricacao, Qt_Doses) 
-    -- CORRIGIDO: Usando o formato 'YYYYMMDD'
     VALUES ('LOT-VENC', 'VAC-GRIPE', 'FAB-BTN', '20240520', '20230520', 50);
 END
 
+-- 1.1. Inserindo o Registro com Vacina Vencida
 IF NOT EXISTS (SELECT 1 FROM dbo.Vacinacao_Registro WHERE Cd_Vacinacao = 'REG-VENC')
 BEGIN
     INSERT INTO dbo.Vacinacao_Registro (Cd_Vacinacao, Dt_Vacinacao, Nu_Dose, Cd_Paciente, Cd_Vacinador, Cd_Unidade, Cd_Lote)
-    -- CORRIGIDO: Usando o formato 'YYYY-MM-DDTHH:MI:SS'
     VALUES ('REG-VENC', '2024-06-01T10:00:00', 1, 'PAC-001', 'VACD-003', 'UNID-CPA', 'LOT-VENC');
 END
 GO
 
 
--- Passo 2: Criar o Stored Procedure.
+
 DROP PROCEDURE IF EXISTS dbo.spPacientesComVacinaVencida;
 GO
 
-PRINT '--- Criando o Stored Procedure dbo.spPacientesComVacinaVencida... ---';
-GO 
 
+-- 2. Create Stored Procedure (dbo.spPacientesComVacinaVencida)
 CREATE PROCEDURE dbo.spPacientesComVacinaVencida
 AS
 BEGIN
@@ -46,12 +42,11 @@ BEGIN
     JOIN
         dbo.Vacina AS v ON l.Cd_Vacina = v.Cd_Vacina
     WHERE
-        vr.Dt_Vacinacao > l.Dt_Validade;
+        vr.Dt_Vacinacao > l.Dt_Validade; -- Data depois da Validade
 END
 GO
 
 
--- Passo 3: Executar (chamar) o Stored Procedure.
-PRINT '--- Executando o Stored Procedure para gerar o relatório. ---';
+-- 3. Exec Stored Procedure do Select de Pacientes com Vacina Vencida
 EXEC dbo.spPacientesComVacinaVencida;
 GO
